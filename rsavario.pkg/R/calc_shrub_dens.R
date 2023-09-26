9de9da9b930c513dc768656986415af91e8b881f
ethan savario <ethan.savario@gmail.edu>
2023-09-26 19:09:24 +0000

    nvm

--- rsavario.pkg/R/calc_shrub_dens.R
+++ rsavario.pkg/R/calc_shrub_dens.R
@@ -2,5 +2,5 @@ calc_shrub_dens<-function(height,length,width){
   volume<-calc_shrub_vol(height,length,width)
   mass<-est_shrub_mass(volume)
   density<-mass/volume
-return(denstiy)
+return(density)
   }

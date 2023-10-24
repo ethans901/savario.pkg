14aefa210593affcfd5a40f6a720aed2a8b1401c
ethan savario <ethan.savario@gmail.edu>
2023-10-24 20:30:32 +0000

    push test

--- rsavario.pkg/R/AreaOfCrab.R
+++ rsavario.pkg/R/AreaOfCrab.R
@@ -1,3 +1,15 @@
+#'Measures area of carapace 
+#'
+#'Takes the length and width of the carapace of the crab and multiplies them. 
+#'Returns the area of the carapace.
+#' 
+#'@param data .csv file as a data frame
+#'@param column1 numerical column of length measurements 
+#'@param column2 numerical column of width measurements
+#'@return area Value from column 1 multiplied by column 2 (numeric) 
+#' 
+#'@export
+ 
 crab_area<-function(data,column1,column2){
   new_col<-data %>% 
     na.omit(data) %>% 

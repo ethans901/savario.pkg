5e565d8c5941d3097c4f622109bbb2d108edef86
ethan savario <ethan.savario@gmail.edu>
2023-10-24 20:35:10 +0000

    testing new version of code

--- rsavario.pkg/R/LinearModel.R
+++ rsavario.pkg/R/LinearModel.R
@@ -5,3 +5,12 @@ linear_model_crabs<-function(data,col1,col2){
 #This function is to calculate and a linear model and provide a summary of the model. 
 #This allows us to see if two variable in a data set are related linearly.
 #(For example, in the crabs data set, this function was used to determine if color and Latitude were related.)
+
+area_vs_latitude<-function(data,column1,column2,column3){
+  area<-data %>% 
+    na.omit(data) %>% 
+    mutate(area={{column1}}*{{column2}})
+  crab_fit<-lm(area~{{column3}},data) %>% 
+    summary(crab_fit)
+  return(crab_fit)
+}
